b24cd135bfc6b3738d742299923086a095d25af6
naryeong-ko <naryung26@gmail.com>
2022-05-23 23:25:36 +0900

    init: 프로젝트 구조 세팅

--- PairFE/vue-pair-fe/src/api/user.js
+++ PairFE/vue-pair-fe/src/api/user.js
@@ -1,31 +1,43 @@
 import {api} from '@/api/index'
+const REST_API = `http://localhost:9999`
 
 //유저 회원가입
 export function join(userData) {
-    return api.post('/api/user/join', userData)
+    // api.defaults.headers
+    return api.post(`${REST_API}/api/user/join`, { headers: { 'Content-Type': 'multipart/form-data' }}, userData)
 }
 
 //유저 로그인
 export function login(loginId, loginPw) {
-    return api.post('/api/user/login', loginId, loginPw)
-}
-
-//유저 로그아웃
-export function logout() {
-    return api.get('/api/user')
+    return api.post(`${REST_API}/api/user/login`, loginId, loginPw)
 }
 
 //유저 수정
-export function updateUser(userId, userData) {
-    return api.patch(`/api/user/${userId}`, userData)
+export function updateUser(userData) {
+    return api.patch(`${REST_API}/api/user`, userData)
 }
 
 //유저 탈퇴
 export function deleteUser(userId) {
-    return api.delete(`/api/user/${userId}`)
+    return api.delete(`${REST_API}/api/user/${userId}`)
 }
 
 //유저 상세 조회
 export function getUser(userId) {
-    return api.get(`/api/user/${userId}`)
+    return api.get(`${REST_API}/api/user/${userId}`)
+}
+
+//유저 전체 조회
+export function getUserList() {
+    return api.get(`${REST_API}/api/users`)
+}
+
+//팔로잉 목록 조회
+export function getFollowingList(userId) {
+    return api.get(`${REST_API}/api/users/following/${userId}`)
+}
+
+//팔로우 목록 조회
+export function getFollowerList(userId) {
+    return api.get(`${REST_API}/api/users/follower/${userId}`)
 }
