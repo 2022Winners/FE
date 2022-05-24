d6fd0745c823703f9dcb3a4fc88af9aef8b6741a
hyungi4608 <hyungi4608@gmail.com>
2022-05-25 02:25:15 +0900

    feat : 연령, 성별 좋아요 누른 추천 영상 리스트

--- PairFE/vue-pair-fe/src/views/MainView.vue
+++ PairFE/vue-pair-fe/src/views/MainView.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
     <h2>메인 뷰</h2>
+    <recommend-gender></recommend-gender>
+    <recommend-age></recommend-age>
     <router-link to="/post">영상뷰</router-link>
-
+     
     <router-view/>
   </div>
   
 </template>
 
 <script>
+import RecommendGender from "@/components/post/RecommendGender";
+import RecommendAge from "@/components/post/RecommendAge";
 
 export default {
    name: "MainView",
    components: {
-    
+      RecommendGender, RecommendAge
    }
 }
 </script>
